0b8217feffa976a523c65c50502ff5a1968c4fa8
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-13 16:49:16 +0900

    Define functions

--- detect_json.py
+++ detect_json.py
@@ -33,89 +33,96 @@ def draw_box(image, box:list, color=(0, 255, 0), thickness=2):
     
     return image
 
+#Define a function predict the bounding box of the image
+def predict(input_dir:Path,output_dir:Path,path_weight='best.pt'):
 
-#Get device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#print device
-print("Device: ", device)
+    #Get device
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #print device
+    print("Device: ", device)
 
-#Load the model from torch hub
+    #Load the model from torch hub
 
-#Create weights veriable and assign path of weights model
-path_weights = 'best.pt'
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_weights)
+    #Create weights veriable and assign path of weights model
+    path_weights = 'best.pt'
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=path_weights)
 
-#Input directory
-input_dir = '/media/buntuml/DATASET/TEST_CASE/dam/wall/1'
-#Convert input directory to pathlib 
-input_dir = Path(input_dir)
+    #Convert input directory to pathlib 
+    input_dir = Path(input_dir)
 
-#Create image list
-image_list = list(input_dir.glob('*.JPG'))
-
-#Create a dataset directory
-DATASET_DIR = Path('dataset_json')
-make_dirs(DATASET_DIR)
-
-#Loop over the image list
-for path in tqdm(image_list[10:]):
-    #Get label id 
-    label_id = path.stem.split('.')[0]
-    #Read image
-    # image = cv2.imread(str(path))
-    image = Image.open(path)
-    #Get prediction from model
-    prediction = model(image)
-    #Get bounding boxes
-    if prediction.pred[0].shape[0]:
-        #Copy image to dataset directory
-        input_img_path = path 
-        output_img_path = Path(DATASET_DIR)/'images'/f'{path.stem}.jpg'
-        shutil.copy(input_img_path,output_img_path)
+    #Create image list
+    image_list = list(input_dir.glob('*.JPG'))
 
 
-        #Get image width and height
-        width, height = image.size
-        
+    #Loop over the image list
+    for path in tqdm(image_list[10:]):
+        #Get label id 
+        label_id = path.stem.split('.')[0]
+        #Read image
+        # image = cv2.imread(str(path))
+        image = Image.open(path)
+        #Get prediction from model
+        prediction = model(image)
+        #Get bounding boxes
+        if prediction.pred[0].shape[0]:
+            #Copy image to dataset directory
+            input_img_path = path 
+            output_img_path = Path(output_dir)/'images'/f'{path.stem}.jpg'
+            shutil.copy(input_img_path,output_img_path)
 
-        #Create a annotations dictionary
-        annotations = {
-            "annotations": [],
-        }
-        #Add images key to annotations dictionary
-        annotations["images"] = [{
-            'id':1,
-            'file_name': str(output_img_path.name),
-            'height': height,
-            'width': width
 
-        }]
-        #Loop over the prediction
-        for pred in prediction.xywh[0]:
-            #Get bounding box: [x_center, y_center, width, height]
-            x_center, y_center, width, height,confidence,class_id = pred
-            #Convert x_center, y_center, width, height to xywh
-            x_min = int(x_center - width/2)
-            y_min = int(y_center - height/2)
-            width = int(width)
-            height = int(height)
-            #Create a bounding box list 
-            bbox = [x_min, y_min, width, height]
+            #Get image width and height
+            width, height = image.size
             
-            #add bounding box to annotations dictionary
-            annotations["annotations"].append({
-                "id": 1,
-                "image_id": 1,
-                "category_id": str(class_id.item()),
-                "bbox": bbox,
-                "score": float(confidence)
-                })
-        #Create a label dictionary
 
-        prediction.files = [path.name]
-        #Save prediction to file
-        prediction.save('output')
-        #Save annotations to json file
-        with open(f'{DATASET_DIR}/labels/{label_id}.json','w',encoding='utf8') as f:        
-            json.dump(annotations, f, ensure_ascii=False)
+            #Create a annotations dictionary
+            annotations = {
+                "annotations": [],
+            }
+            #Add images key to annotations dictionary
+            annotations["images"] = [{
+                'id':1,
+                'file_name': str(output_img_path.name),
+                'height': height,
+                'width': width
 
+            }]
+            #Loop over the prediction
+            for pred in prediction.xywh[0]:
+                #Get bounding box: [x_center, y_center, width, height]
+                x_center, y_center, width, height,confidence,class_id = pred
+                #Convert x_center, y_center, width, height to xywh
+                x_min = int(x_center - width/2)
+                y_min = int(y_center - height/2)
+                width = int(width)
+                height = int(height)
+                #Create a bounding box list 
+                bbox = [x_min, y_min, width, height]
+                
+                #add bounding box to annotations dictionary
+                annotations["annotations"].append({
+                    "id": 1,
+                    "image_id": 1,
+                    "category_id": str(class_id.item()),
+                    "bbox": bbox,
+                    "score": float(confidence)
+                    })
+            #Create a label dictionary
+
+            prediction.files = [path.name]
+            #Save prediction to file
+            prediction.save('output')
+            #Save annotations to json file
+            with open(f'{output_dir}/labels/{label_id}.json','w',encoding='utf8') as f:        
+                json.dump(annotations, f, ensure_ascii=False)
+
+
+#Run the main function
+if __name__ == '__main__':
+    #Input directory
+    input_dir = '/media/buntuml/DATASET/TEST_CASE/dam/wall/1'
+    #Create a dataset directory
+    output_dir = Path('dataset_json')
+    make_dirs(output_dir)
+    #Predict bounding box
+    predict(input_dir=input_dir,output_dir=output_dir)
